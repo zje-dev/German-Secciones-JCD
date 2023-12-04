bbf3b554980df8ee013ea0d25bb99ec7b7a7c6b5
zje-dev <50305531+zje-dev@users.noreply.github.com>
2023-12-04 10:38:35 -0500

    Add files via upload

--- seccion7/ejercicio 1/main.js
+++ seccion7/ejercicio 1/main.js
@@ -1,4 +1,8 @@
-let array = [0, 1, 2, 3]
-for (let i = 0; i < 4; i++) {
-    console.log(array[i])
+function show_array_elements() {
+    let array = [0, 1, 2, 3]
+    for (let i = 0; i < 4; i++) {
+        console.log(array[i])
+    }
 }
+
+show_array_elements()
--- seccion7/ejercicio 2/main.js
+++ seccion7/ejercicio 2/main.js
@@ -1,8 +1,11 @@
-let array = [0, 1, 2, 3, 4, 5, 6, 7, 9]
-for (let i = 0; i < 9; i++) {
-    console.log(array[i])
+function straight_backwards_array() {
+    let array = [0, 1, 2, 3, 4, 5, 6, 7, 9]
+    for (let i = 0; i < 9; i++) {
+        console.log(array[i])
+    }
+    array.reverse()
+    for (let i = 0; i < 9; i++) { //Una alternative sería definir i como 9 y reemplazar el i++ por i--, para que así empiece por el final
+        console.log(array[i])
+    }
 }
-array.reverse()
-for (let i = 0; i < 9; i++) { //Una alternative sería definir i como 9 y reemplazar el i++ por i--, para que así empiece por el final
-    console.log(array[i])
-}
+straight_backwards_array()
